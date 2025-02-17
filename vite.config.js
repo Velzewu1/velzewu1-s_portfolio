0ac599498ddb833328b42fcbf8a5ca3c48b35574
Velzewu1 <nsr.samatov@gmail.com>
2025-02-18 01:30:42 +0500

    Character, camera movement. Test sprites.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { writeFileSync } from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/velzewu1-s_portfolio/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  // Создаёт .nojekyll после билда
+  buildEnd: () => {
+    writeFileSync(resolve(__dirname, "dist/.nojekyll"), "");
+  },
+});
