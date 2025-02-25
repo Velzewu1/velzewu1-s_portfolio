f01942a8b40803e3817741e0d702294b1bffeca3
Velzewu1 <nsr.samatov@gmail.com>
2025-02-26 01:56:52 +0500

    NOT FOUND error during updates fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GamePage from './pages/GamePage';
 import AboutPage from './pages/AboutPage';
