f67fb1115d7b654e3eccacd214c2751518e781ac
Саматов Ансар Нурланоаич <171016665@step.kz>
2025-02-27 21:20:22 +0500

    JSON fix

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [
-    react(),
-  ],
-  base: "/velzewu1-s_portfolio"
-=======
   plugins: [ react() ],
   define: {
     global: 'window'
   }
->>>>>>> ed0fd1ef525220f7430872aa4a81397d1a18bd22
 })
